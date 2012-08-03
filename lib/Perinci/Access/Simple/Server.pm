6a6cd4080d0b7e0a6a6d5a804ace7125fefa1b20
Steven Haryanto (on PC) <stevenharyanto@gmail.com>
2012-08-03 22:44:00 +0700

    Add main module to avoid breaking, e.g. Task::BeLike::SHARYANTO::Authored

---
+++ lib/Perinci/Access/Simple/Server.pm
@@ -0,0 +1,12 @@
+package Perinci::Access::Simple::Server;
+
+# VERSION
+
+1;
+# ABSTRACT: Riap::Simple server implementations
+
+=head1 SEE ALSO
+
+L<Perinci::Access::Simple::Server::Pipe>
+
+L<Perinci::Access::Simple::Server::Socket>
