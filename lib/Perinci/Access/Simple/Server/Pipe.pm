1109eef8bfa5ad819c46d51225494635be5ea46f
perlancar (on netbook-zenbook-ux305) <perlancar@gmail.com>
2017-07-03 22:50:35 +0700

    Switch from Log::Any to Log::ger

--- lib/Perinci/Access/Simple/Server/Pipe.pm
+++ lib/Perinci/Access/Simple/Server/Pipe.pm
@@ -6,7 +6,7 @@ package Perinci::Access::Simple::Server::Pipe;
 use 5.010001;
 use strict;
 use warnings;
-use Log::Any '$log';
+use Log::ger;
 
 use Data::Clean::FromJSON;
 use Data::Clean::JSON;
@@ -44,7 +44,7 @@ sub handle {
 sub send_response {
     my $self = shift;
     my $res = $self->res // [500, "BUG: Response not set"];
-    $log->tracef("Sending response to stdout: %s", $res);
+    log_trace("Sending response to stdout: %s", $res);
     my $v = $self->req->{v} // 1.1;
     insert_riap_stuffs_to_res($res, $v);
     $res = $cleanser->clone_and_clean($res);
@@ -55,12 +55,12 @@ sub run {
     my $self = shift;
     my $last;
 
-    $log->tracef("Starting loop ...");
+    log_trace("Starting loop ...");
 
   REQ:
     while (1) {
         my $line = <STDIN>;
-        $log->tracef("Read line from stdin: %s", $line);
+        log_trace("Read line from stdin: %s", $line);
         last REQ unless defined($line);
         my $req_json;
         if ($line =~ /\Aj(.*)/) {
@@ -70,7 +70,7 @@ sub run {
             $last++;
             goto RES;
         }
-        $log->tracef("Read JSON from stdin: %s", $req_json);
+        log_trace("Read JSON from stdin: %s", $req_json);
         my $req;
         eval {
             $req = $json->decode($req_json);
